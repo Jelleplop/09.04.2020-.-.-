80f16d88e0b14f492a9be5205e3b7c29fa7f4a4b
Jelleplop <Jelleplop@FX>
2020-04-09 09:24:57 +0400

    Добавьте файлы проекта.

---
+++ 09.04.2020 Марикн. И/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _09._04._2020_Марикн._И
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] one = new byte[5];
+            one[0] = 56;
+            one[2] = 200;
+
+            Console.WriteLine(one[2]);
+
+            string[] two = new string[] { "Bob", "Jeb", "Iliya", "I" };
+
+            Console.WriteLine(two[1]);
+
+            short[,] nums = new short[5, 2];
+
+            int[,] nubmers = 
+            {
+             { 13, 10, 235 },
+             { 1, 300, 6},
+             {7, 0, 82 }
+            } ;
+
+            nubmers[1, 1] = 500;
+
+            Console.WriteLine(nubmers[1, 1]);
+
+            int len = two.Length;
+            Console.WriteLine(len);
+
+            Console.WriteLine(two[1]);
+
+            string[] copynew = new string[len];
+
+            two.CopyTo(copynew, 0);
+
+            Console.WriteLine(copynew[1]);
+
+        }
+    }
+}
